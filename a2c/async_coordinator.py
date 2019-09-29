c20c1df1ec3ea2ead711c28bcb7017205d6787f9
Jon Deaton <jonpauldeaton@gmail.com>
2019-09-28 18:23:22 -0700

    spawn worker process before importing tensorflow....

--- a2c/async_coordinator.py
+++ a2c/async_coordinator.py
@@ -4,62 +4,48 @@ Date: 9/21/19
 Author: Jon Deaton (jonpauldeaton@gmail.com)
 """
 
-
-
-from a2c.rollout import Rollout
-from multiprocessing import Process, Queue, Value
-
-
-def worker_task(wid, queue: Queue, get_rollout, make_model, get_env, model_directory):
-    """ the task that each worker process performs: gather the complete
-     roll-out of an episode using the latest model and send it back to the
-     master process. """
-    env = get_env()
-    model = make_model()
-
-    while True:
-        model.load_weights(model_directory)
-
-        print(f"Worker {wid} starting episode...")
-        rollout: Rollout = get_rollout(model, env)
-        print(f"Worker {wid} episode finished")
-
-        queue.put(rollout)
-
+from multiprocessing import Process, Queue, Condition, Semaphore
 
 class AsyncCoordinator:
     """ asynchronous remote  """
-    def __init__(self, num_envs, model_directory, make_model, get_env, get_rollout):
-        self.num_envs = num_envs
-        self.model_directory = model_directory
-        self.get_rollout = get_rollout
-        self.make_model = make_model
-        self.get_env = get_env
+    def __init__(self, num_workers, worker_target, args):
+        self.num_workers = num_workers
+        self.worker_target = worker_target
+        self.args = args
 
         self.queue = None
+        self.sema = None
+        self._workers = None
 
-    def __enter__(self):
+    def open(self):
         self.queue = Queue()
-        self.workers = []
-        for w in range(self.num_envs):
+        self.sema = Semaphore(0)
 
-            args = (w, self.queue, self.get_rollout, self.make_model, self.get_env, self.model_directory)
-
-            worker = Process(target=worker_task, args=args)
+        self._workers = []
+        for wid in range(self.num_workers):
+            worker = Process(target=self.worker_target,
+                             args=(wid, self.queue, self.sema) + self.args)
             worker.start()
-            self.workers.append(worker)
+            self._workers.append(worker)
 
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def close(self):
         del self.queue
-        for worker in self.workers:
+        for worker in self._workers:
             worker.terminate()
             worker.join()
 
-    def await_rollout(self):
+    def start(self):
+        for _ in range(self.num_workers):
+            self.sema.release()
+
+    def pop(self):
         if self.queue is None:
-            raise Exception
+            raise Exception()
         return self.queue.get()
 
+    def __enter__(self):
+        self.open()
+        return self
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
--- a2c/training.py
+++ a2c/training.py
@@ -12,39 +12,78 @@ Inspired by:
 from a2c.hyperparameters import HyperParameters
 from a2c.rollout import Rollout, transpose_batch
 from a2c.async_coordinator import AsyncCoordinator
-from a2c.eager_models import ConvLSTMAC, LSTMAC, get_encoder_type
-from a2c.losses import *
 
 import os
 from tqdm import tqdm
 import numpy as np
-import tensorflow as tf
-import tensorflow.keras.optimizers as ko
-
 import logging
 logger = logging.getLogger("root")
 logger.propagate = False
+
+from multiprocessing import Queue, Semaphore
+
+
+def make_model(input_shape, architecture, encoder_class, action_shape):
+    """ build the actor-critic network """
+    from a2c.eager_models import get_encoder_type
+
+    Encoder = get_encoder_type(encoder_class)
+
+    if architecture == 'LSTM':
+        from a2c.eager_models import LSTMAC
+        model = LSTMAC(input_shape, action_shape, Encoder)
+
+    elif architecture == 'ConvLSTM':
+        from a2c.eager_models import ConvLSTMAC
+        model = ConvLSTMAC(action_shape)
+    else:
+        raise ValueError(f"Unknown architecture: {architecture}")
+    return model
+
+
+def worker_target(wid: int, queue: Queue, sema: Semaphore,
+                  get_env, model_directory, to_action,
+                  hyperams: HyperParameters):
+    """ the task that each worker process performs: gather the complete
+         roll-out of an episode using the latest model and send it back to the
+         master process. """
+    env = get_env()
+
+    input_shape = (None,) + env.observation_space.shape
+    model = make_model(input_shape,
+                       hyperams.architecture,
+                       hyperams.encoder_class,
+                       hyperams.action_shape)
+
+    sema.acquire()  # wait for master process to signal
+
+    while True:
+        model.load_weights(model_directory)
+
+        print(f"Worker {wid} starting episode...")
+        rollout = get_rollout(model, env,
+                              hyperams.agents_per_env,
+                              hyperams.episode_length,
+                              to_action)
+        print(f"Worker {wid} episode finished")
+        queue.put(rollout)
 
 
 def get_rollout(model, env, agents_per_env, episode_length, to_action,
                 record=True, progress_bar=True) -> Rollout:
     """ performs a roll-out """
+    import tensorflow as tf
     rollout = Rollout() if record else None
 
     observations = env.reset()
     dones = [False] * agents_per_env
 
-    print("resetting states...")
     model.reset_states()
 
     iter = tqdm(range(episode_length)) if progress_bar else range(episode_length)
     for _ in iter:
         if all(dones): break
-
-        print("converting observations to tensor...")
         obs_tensor = tf.expand_dims(tf.convert_to_tensor(observations), axis=1)
-
-        print("getting actions...")
         action_logits, est_values = model(obs_tensor)
 
         # sample the action
@@ -61,21 +100,6 @@ def get_rollout(model, env, agents_per_env, episode_length, to_action,
     return rollout
 
 
-def make_model(input_shape, architecture, encoder_class, action_shape):
-    """ build the actor-critic network """
-    Encoder = get_encoder_type(encoder_class)
-
-    if architecture == 'LSTM':
-        model = LSTMAC(input_shape, action_shape, Encoder)
-
-    elif architecture == 'ConvLSTM':
-        model = ConvLSTMAC(action_shape)
-
-    else:
-        raise ValueError(architecture)
-
-    return model
-
 
 
 class Trainer:
@@ -85,25 +109,8 @@ class Trainer:
         self.hyperams = hyperams
         self.to_action = to_action
         self.test_env = test_env
-
         self.num_envs = hyperams.num_envs
-
-        self._set_seed(hyperams.seed)
-
-        self.input_shape = (None,) + self.get_env().observation_space.shape
-        self.model = make_model(self.input_shape,
-                                self.hyperams.architecture,
-                                self.hyperams.encoder_class,
-                                self.hyperams.action_shape)
-
-        self.optimizer = ko.Adam(lr=hyperams.learning_rate, clipnorm=1.0)
-
         self.training_dir = training_dir
-        self.tensorboard = None
-        if training_dir is not None:
-            self.summary_writer = tf.summary.create_file_writer(training_dir)
-            self.tensorboard = tf.keras.callbacks.TensorBoard(log_dir=training_dir,
-                                                              histogram_freq=1)
 
     def train(self, asynchronous=False):
         """ trains a model """
@@ -126,72 +133,75 @@ class Trainer:
 
     def _train_async(self):
         """ trains a model asynchronously """
-        if self.training_dir is None:
-            raise ValueError
-
-        def _get_rolllout(model, env):
-            """ closure alias for get_rollout with captured hyperparameters """
-            return get_rollout(model, env, self.hyperams.agents_per_env,
-                         self.hyperams.episode_length, self.to_action)
-
-        def _make_model():
-            return make_model(self.input_shape,
-                              self.hyperams.architecture,
-                              self.hyperams.encoder_class,
-                              self.hyperams.action_shape)
 
         model_directory = os.path.join(self.training_dir, "model")
 
-        logger.info("Saving initial model...")
-        self.model.save_weights(model_directory)
-        logger.info("Initial model saved.")
-
-        coordinator = AsyncCoordinator(self.hyperams.num_envs, model_directory,
-                                       _make_model, self.get_env, _get_rolllout)
+        args = self.get_env, model_directory, self.to_action, self.hyperams
+        coordinator = AsyncCoordinator(self.hyperams.num_envs,
+                                       worker_target, args)
 
         with coordinator:
+            import tensorflow as tf
+
+            input_shape = (None,) + self.get_env().observation_space.shape
+            model = make_model(input_shape,
+                               self.hyperams.architecture,
+                               self.hyperams.encoder_class,
+                               self.hyperams.action_shape)
+
+            self.optimizer = tf.keras.optimizers.Adam(lr=self.hyperams.learning_rate, clipnorm=1.0)
+
+            logger.info("Saving initial model...")
+            model.save_weights(model_directory)
+            logger.info("Initial model saved.")
+
+            coordinator.start()  # start the worker processes
+
             for ep in range(self.hyperams.num_episodes):
-                rollout = coordinator.await_rollout()
+                rollout = coordinator.pop()
                 self._log_rollout(rollout, ep, self.hyperams.episode_length)
                 self._update_with_rollout(rollout)
-                self.model.save_weights(model_directory)
+                model.save_weights(model_directory)
 
     def _train_async_shared(self):
         """ trains asynchronously with a single shared model
         todo: this is interesting but ultimately garbage... try make better someday?
         """
-        from a2c.shared_async_coordinator import AsyncCoordinatorShared
-        import threading
-        lock = threading.Lock()
-
-        def get_actions(observations, carry):
-            obs_in = tf.convert_to_tensor(observations)
-            with lock:
-                actions_t, values_t, carry = self.model.cell.action_value(obs_in, carry)
-
-            # convert Tensors to list of action-indices and values
-            actions = actions_t.numpy()
-            values = list(values_t.numpy())
-
-            return actions, values, carry
-
-        def initialize():
-            with lock:
-                return self.model.cell.get_initial_state(batch_size=self.hyperams.agents_per_env,
-                                                   dtype=tf.float32)
-
-        coordinator = AsyncCoordinatorShared(self.hyperams.num_envs, self.get_env,
-                                       self.hyperams.episode_length,
-                                       initialize, get_actions, self.to_action)
-
-        with coordinator:
-            for ep in range(self.hyperams.num_episodes):
-                rollout = coordinator.await_rollout()
-                self._log_rollout(rollout, ep)
-                self._update_with_rollout(rollout)
+        # from a2c.shared_async_coordinator import AsyncCoordinatorShared
+        # import threading
+        # lock = threading.Lock()
+        #
+        # def get_actions(observations, carry):
+        #     obs_in = tf.convert_to_tensor(observations)
+        #     with lock:
+        #         actions_t, values_t, carry = self.model.cell.action_value(obs_in, carry)
+        #
+        #     # convert Tensors to list of action-indices and values
+        #     actions = actions_t.numpy()
+        #     values = list(values_t.numpy())
+        #
+        #     return actions, values, carry
+        #
+        # def initialize():
+        #     with lock:
+        #         return self.model.cell.get_initial_state(batch_size=self.hyperams.agents_per_env,
+        #                                            dtype=tf.float32)
+        #
+        # coordinator = AsyncCoordinatorShared(self.hyperams.num_envs, self.get_env,
+        #                                self.hyperams.episode_length,
+        #                                initialize, get_actions, self.to_action)
+        #
+        # with coordinator:
+        #     for ep in range(self.hyperams.num_episodes):
+        #         rollout = coordinator.await_rollout()
+        #         self._log_rollout(rollout, ep)
+        #         self._update_with_rollout(rollout)
 
     def _update_with_rollout(self, rollout):
         """ updates the network using a roll-out """
+        import tensorflow as tf
+        from a2c.losses import a2c_loss, make_returns_batch
+
         observations, actions, rewards, values, dones = rollout.as_batch()
 
         returns = make_returns_batch(rewards, self.hyperams.gamma)
@@ -210,17 +220,20 @@ class Trainer:
         logger.info(f"Applying gradients...")
         self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
 
+
     def _log_rollout(self, rollout, episode, episode_length):
         """ logs the performance of the roll-out """
         logger.info(f"Episode {episode}")
 
         for rewards in transpose_batch(rollout.rewards):
+            G = rewards.sum()
             mass = rewards.cumsum() + 10
             eff = get_efficiency(rewards, episode_length, self.hyperams)
             print(f"Return:\t{G:.0f}, max mass:\t{mass.max():.0f}, avg. mass:\t{mass.mean():.1f}, efficiency:\t{eff:.1f}")
 
 
     def test(self, episode_length=None):
+        return
         logger.info(f"Testing performance...")
         episode_length = episode_length or self.hyperams.episode_length
         rollout = get_rollout(self.model, self.test_env,
@@ -228,11 +241,6 @@ class Trainer:
                               episode_length, self.to_action)
 
         self._log_rollout(rollout, "test", episode_length)
-
-    def _set_seed(self, seed):
-        # todo: this isn't everywhere that it needs to be set
-        np.random.seed(seed)
-        tf.random.set_seed(seed)
 
 
 def get_efficiency(rewards, episode_length, hyperams):
