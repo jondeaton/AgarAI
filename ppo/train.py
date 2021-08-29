ce5603a9fc0e3295b51a02dd3a9f2d66a3fa1ac5
Jon Deaton <jon@Jons-MacBook-Pro.local>
2021-08-29 11:43:56 -0700

    Make jittable action converter.

--- ppo/train.py
+++ ppo/train.py
@@ -28,67 +28,101 @@ tf.config.experimental.set_visible_devices([], "GPU")  # no GPU for U, bitch.
 # todo: Do I need this???
 # os.environ["XLA_FLAGS"] = "--xla_cpu_multi_thread_eigen=false intra_op_parallelism_threads=1"
 
-
-def agario_to_action(index, action_shape):
-    """ Converts a raw action index into an Agario action """
-    if index is None:
-        return None
-    if type(index) is not int:
-        index = int(index)
-    indices = np.unravel_index(index, action_shape)
-    theta = (2 * np.pi * indices[0]) / action_shape[0]
-    mag = 1 - indices[1] / action_shape[1]
-    act = int(indices[2])
-    x = np.cos(theta) * mag
-    y = np.sin(theta) * mag
-    return np.array([x, y]), act
-
-
 def action_size(action_config: environment_pb2.Action) -> int:
-    # todo: fix this
-    num_acts = int(action_config.allow_splitting) + int(action_config.allow_feeing)
+    num_acts = 1 + int(action_config.allow_splitting) + int(action_config.allow_feeding)
     move_size = action_config.num_directions * action_config.num_magnitudes
     return 1 + move_size + action_config.num_directions * num_acts
 
 
-def get_action_converter(action_config: environment_pb2.Action) -> Callable:
+AgarioAction = Tuple[np.ndarray, int]
+
+
+def make_action_converter(action_config: environment_pb2.Action):
     """Creates function to turn what comes out of the model into an
     action that can go into the environment.
+
+    Actions have the following form
+
+    index 0: do nothing
+    next (num_directions * num_magnitudes) indexes: just move
+    next (num_directions * num_actions): act in specified direction
     """
-    # todo: fix this
-    def index_to_action(indexg: int):
-        return np.array([0, 0]), 0
-        # if index == 0:
-        #     return np.array([0, 0]), 0
-        #
-        #  move_size = action_config.num_directions * action_config.num_magnitudes
-        #  if index + 1 < move_size:
-        #      ...  # ugh
-        #
-        # indices = np.unravel_index(index, action_config.num_directions)
-        # theta = (2 * np.pi * indices[0]) / action_shape[0]
-        # mag = 1 - indices[1] / action_shape[1]
-        # act = int(indices[2])
-        # x = np.cos(theta) * mag
-        # y = np.sin(theta) * mag
-        # return np.array([x, y]), act
+    num_moves = action_config.num_directions * action_config.num_magnitudes
 
-    return index_to_action
+    @jax.jit
+    def direction(magnitude: float, theta: float) -> np.ndarray:
+        x = np.cos(theta) * magnitude
+        y = np.sin(theta) * magnitude
+        return np.array([x, y])
+
+    @jax.jit
+    def move(index: int) -> AgarioAction:
+        theta_index = index % action_config.num_directions
+        mag_index = index // action_config.num_directions
+
+        theta = 2 * np.pi * theta_index / action_config.num_directions
+        mag = 1 - mag_index / (1 + action_config.num_magnitudes)
+        return direction(mag, theta), 0
+
+    @jax.jit
+    def act(index: int) -> AgarioAction:
+        theta_index = index % action_config.num_directions
+        theta = 2 * np.pi * theta_index / action_config.num_directions
+
+        if action_config.allow_splitting and action_config.allow_feeding:
+            act = 1 + index / action_config.num_directions
+        elif action_config.allow_splitting:
+            act = 1
+        elif action_config.allow_feeding:
+            act = 2
+        else:
+            act = 0
+
+        return direction(1.0, theta), act
+
+    @jax.jit
+    def do_something(index: int) -> AgarioAction:
+        return jax.lax.cond(
+            index < num_moves,
+            lambda i: move(i),
+            lambda i: act(i - num_moves),
+            operand=index
+        )
+
+    @jax.jit
+    def action_converter(index: int) -> AgarioAction:
+        do_nothing = np.array([0, 0]), 0
+        return jax.lax.cond(
+            index == 0,
+            lambda _: do_nothing,
+            lambda i: do_something(i - 1),
+            operand=index)
+
+    return action_converter
 
 
-def make_feature_extractor(output_size: int, mode: str) -> Tuple[Callable, Callable]:
-    SiLu = stax.elementwise(lambda x: x * jax.nn.sigmoid(x))
-    return stax.serial(
-        stax.Conv(32, filter_shape=(5, 5), padding='SAME'), stax.BatchNorm(), SiLu,
-        stax.Conv(32, filter_shape=(5, 5), padding='SAME'), stax.BatchNorm(), SiLu,
-        stax.Conv(10, filter_shape=(3, 3), padding='SAME'), stax.BatchNorm(), SiLu, stax.Dropout(0.3, mode=mode),
-        stax.Conv(10, filter_shape=(3, 3), padding='SAME'), SiLu, stax.Dropout(0.3, mode=mode),
-        stax.Flatten,
-        stax.Dense(output_size))
+def make_actor_critic(model_config: config_pb2.Model,
+                      num_actions: int, mode: str):
+    """Makes Actor-Critic model.
 
-
-def make_actor_critic(model_config, num_actions, mode: str):
+    Args:
+        model_config: Model configuration.
+        num_actions: The number of actions.
+        mode: 'train' or 'test'.
+    Returns:
+        Initialization and apply function.
+    """
     num_features = model_config.feature_extractor.num_features
+
+    def make_feature_extractor(output_size: int, mode: str) -> Tuple[Callable, Callable]:
+        SiLu = stax.elementwise(lambda x: x * jax.nn.sigmoid(x))
+        return stax.serial(
+            stax.Conv(32, filter_shape=(5, 5), padding='SAME'), stax.BatchNorm(), SiLu,
+            stax.Conv(32, filter_shape=(5, 5), padding='SAME'), stax.BatchNorm(), SiLu,
+            stax.Conv(10, filter_shape=(3, 3), padding='SAME'), stax.BatchNorm(), SiLu, stax.Dropout(0.3, mode=mode),
+            stax.Conv(10, filter_shape=(3, 3), padding='SAME'), SiLu, stax.Dropout(0.3, mode=mode),
+            stax.Flatten,
+            stax.Dense(output_size))
 
     fe_init, fe_fn = make_feature_extractor(num_features, mode='train')
     actor_init, actor_fn = stax.Dense(num_actions)
@@ -104,7 +138,7 @@ def make_actor_critic(model_config, num_actions, mode: str):
     def apply_fn(params, observations):
         fe_params, actor_params, critic_params = params
         features = fe_fn(fe_params, observations)
-        return actor_fn(features), critic_fn(features)
+        return actor_fn(features), np.squeeze(critic_fn(features))
 
     return init_fn, apply_fn
 
@@ -117,7 +151,7 @@ def train(env: gym.Env, config: configuration.Config,
 
     input_shape = (1,) + env.observation_space.shape
     num_actions = action_size(config.environment.action)
-    to_action = get_action_converter(config.environment.action)
+    action_converter = make_action_converter(config.environment.action)
 
     init_fn, apply_fn = make_actor_critic(config.model, num_actions, 'train')
 
@@ -139,9 +173,10 @@ def train(env: gym.Env, config: configuration.Config,
             if all(dones): break
             action_logits, values_estimate = apply_fn(observations)
             action_indices = jax.random.categorical(key, action_logits)  # Choose actions.
+            # todo: take along isn't right here...
             log_pas = utils.take_along(jax.nn.log_softmax(action_logits), action_indices)
-            values = list(np.squeeze(values_estimate))
-            actions = [to_action(i) for i in action_indices]
+            values = list(values_estimate)
+            actions = jax.vmap(action_converter)(action_indices)
             next_obs, rewards, next_dones, _ = env.step(actions)
 
             roll.record({
@@ -181,6 +216,7 @@ def train(env: gym.Env, config: configuration.Config,
 
             adam_state = adam_update(step, dparams, adam_state)
             step += 1
+
 
 def main():
     args = parse_args()
