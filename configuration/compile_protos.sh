9a206bbd2897f3c5314457ec3fb01209bd13d767
Jon Deaton <jonpauldeaton@gmail.com>
2021-09-07 21:09:24 -0700

    It runs

--- configuration/compile_protos.sh
+++ configuration/compile_protos.sh
@@ -1,3 +1,9 @@
 
-protoc configuration/environment.proto -I=configuration --python_out=configuration
-protoc configuration/config.proto -I=configuration --python_out=configuration
+protoc -I=configuration --python_out=configuration \
+  configuration/environment.proto \
+  configuration/config.proto
+
+
+# Stupid python protos are broken https://github.com/protocolbuffers/protobuf/issues/1491
+sed -i '' 's/import environment_pb2 as environment__pb2/from . import environment_pb2 as environment__pb2/g' \
+  configuration/config_pb2.py
